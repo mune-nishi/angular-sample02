c878b6e4db7746509bf3db462a4158ad3c2e4120
mune-nishi <ni4kage@gmail.com>
2021-09-12 21:32:19 +0900

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
