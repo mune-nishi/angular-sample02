28911ae963d92cbf295ac8fdb22e79d590446d4e
mune-nishi <ni4kage@gmail.com>
2021-09-12 21:49:51 +0900

    1 file modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -6,6 +6,7 @@ import { Input } from '@angular/core';
   templateUrl: './product-alerts.component.html',
   styleUrls: ['./product-alerts.component.css']
 })
+
 eexport class ProductAlertsComponent implements OnInit {
   @Input() product!: Product;
   constructor() { }
