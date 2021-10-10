17bcf5ededb6c9acf5ae315167c36f666da6ead9
mune-nishi <ni4kage@gmail.com>
2021-10-10 22:13:29 +0900

    up

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
@@ -9,7 +9,9 @@ export class CartService {
 
   items: Product[] = [];
 
-  constructor() {}
+  constructor(
+    private http: HttpClient
+  ) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -22,6 +24,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 
 }
